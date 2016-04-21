3d2828ec5fe5f946fa5cbb4fa1a77febfd76a865
Nikolay Rusov <opengledu@gmail.com>
2016-04-21 17:31:09 +0300

    Solution added

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace BreadDelivery
+{
+        internal class Program
+        {
+            private const string RequestUrl = @"https://api-maps.yandex.ru/services/route/2.0/?callback=id_146124731114152263626&lang=ru_RU&token=6d3545d7aa6bfbf98d6a5038715e3dbd&rll=43.981953%2C56.304944~43.9847%2C56.291964&rtm=dtr&results=3";
+
+            private static string Parse(Stream response)
+            {
+                using (var xr = new StreamReader(response))
+                {
+                    //xr.MoveToContent();
+                    //xr.Read();
+                    //xr.Read();
+                    //if (!xr.ReadToDescendant("AdministrativeAreaName"))
+                    //{
+                    //    return "Адрес не найден";
+                    //}
+                    return xr.ReadToEnd();
+                    //var admName = xr.ReadElementContentAsString();
+                    //if (!admName.EndsWith("область"))
+                    //{
+                    //    xr.ReadToDescendant("SubAdministrativeAreaName");
+                    //    return xr.ReadElementContentAsString();
+                    //}
+                }
+            }
+
+            private static string GetRoute(/*string address*/)
+            {
+                var request = (HttpWebRequest)WebRequest.Create(string.Concat(RequestUrl/*, address*/));
+                using (var response = request.GetResponse())
+                {
+                    using (var responseStream = response.GetResponseStream())
+                    {
+                        return Parse(responseStream);
+                    }
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                File.WriteAllText("response.xml", GetRoute());
+                //var appender = File.AppendText("with_regions.csv");
+                ////Console.WriteLine(Parse(new FileStream("ex.xml", FileMode.Open)));
+                //using (var sr = new StreamReader("adres.csv"))
+                //{
+                //    string query;
+
+                //    while (!sr.EndOfStream)
+                //    {
+                //        query = sr.ReadLine();
+                //        appender.Write(query);
+                //        appender.Write(';');
+                //        appender.WriteLine(GetRegion(query));
+                //        appender.Flush();
+                //    }
+                //}
+                //appender.Close();
+            }
+        }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("BreadDelivery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BreadDelivery")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c00a1716-b8a1-4a2e-b705-e58debfb038f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
