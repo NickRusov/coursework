88134eba7d9b8d43208f4ffe93191b4321ed3327
Nikolay Rusov <opengledu@gmail.com>
2016-05-05 16:26:43 +0300

    customer struct added

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadDelivery
+{
+    struct Customer
+    {
+        public int Id;
+        public string Address;
+        public bool hasEuroLot;
+        public float Demand;
+        public float Latitude;
+        public float Longitude;
+    }
+}
